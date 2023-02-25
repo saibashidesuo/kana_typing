f23f684da06737e89368e4da227a6bbd2b772b65
saibashidesuo <ultrataihen@gmail.com>
2023-02-25 16:12:24 +0900

    refactor 初期画面の文言修正

--- src/initial/Initial.tsx
+++ src/initial/Initial.tsx
@@ -1,3 +1,5 @@
+import { numberOfThemes } from '../env';
+
 type Props = {
   start: () => void;
 };
@@ -6,6 +8,8 @@ export default function Initial({ start }: Props) {
   return (
     <div>
       <h1>かな入力タイピング</h1>
+      <p>全部で{numberOfThemes}問あります！</p>
+      <p>表示された文字を「かな入力」でタイピングしてください！</p>
 
       <div>※始めるボタンを押した瞬間に始まります！</div>
       <button type="button" onClick={start}>
